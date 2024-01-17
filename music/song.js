0b9dcdb042b0b127506afd2fbcb9a27e9c319f45
Trần Kim Cương <trankimcuong301@gmail.com>
2024-01-17 23:49:03 +0700

    update js

---
+++ music/song.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: '故梦',
+        path: './music/故梦 - 双笙.mp3',
+        artist: '双笙',
+        image: './music/img/故梦.jpg'
+    },
+    {
+        name: 'Love Is Gone',
+        path: './music/Love is Gone - SLANDER.mp3',
+        artist: 'SLANDER',
+        image: './music/img/LoveIsGone.jpg'
+    },
+     
+]
