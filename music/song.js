dc22b17b0d968d36d1880079d5c27ebdc00e5138
Hieuuu04 <minhhieu2004005@gmail.com>
2024-01-18 14:15:43 +0700

    fix html

--- music/song.js
+++ music/song.js
@@ -11,5 +11,64 @@ let songs = [
         artist: 'SLANDER',
         image: './music/img/LoveIsGone.jpg'
     },
-     
+    {
+        name: 'Arian',
+        path: './music/arian.mp3',
+        artist: 'Arian',
+        image: './music/img/arian.jpg'
+    }, 
+    {
+        name: 'Buồn hay vui',
+        path: './music/BUỒN HAY VUI.mp3',
+        artist: 'VSOUL x MCK',
+        image: './music/img/buonhayvui.jpg'
+    }, 
+    {
+        name: 'Chẳng còn gì',
+        path: './music/Chẳng Còn Gì.mp3',
+        artist: 'Salmon',
+        image: './music/img/Salmon.png'
+    }, 
+    {
+        name: 'Dân chơi sao phải khóc',
+        path: './music/Dân chơi sao phải khóc.mp3',
+        artist: 'Andree x Ryder',
+        image: './music/img/danchoisaophaikhoc.jpg'
+    },
+    {
+        name: 'GREEN LOVE',
+        path: './music/GREEN LOVE.mp3',
+        artist: 'QNT',
+        image: './music/img/qnt.jpg'
+    }, 
+    {
+        name: 'Lovin On Me',
+        path: './music/Lovin On Me.mp3',
+        artist: 'Jack Harlow',
+        image: './music/img/lovinonme.jpg'
+    }, 
+    {
+        name: 'Những lời hứa bỏ quên',
+        path: './music/NHỮNG LỜI HỨA BỎ QUÊN.mp3',
+        artist: 'Vũ',
+        image: './music/img/nhungloihuaboquen.jpg'
+    },
+    {
+        name: 'Wn id 2022 ft 267',
+        path: './music/Wn  id 2022 ft 267.mp3',
+        artist: 'W/n ',
+        image: './music/img/wn.jpg'
+    },
+    {
+        name: 'Tại vì sao',
+        path: './music/Tại vì sao.mp3',
+        artist: 'MCK ',
+        image: './music/img/mck.jpg',
+    },
+    {
+        name: 'Bé ơi từ từ',
+        path: './music/y2mate.com - WREN EVANS  bé ơi từ từ  LOI CHOI The First Album ft itsnk.mp3',
+        artist: 'WREN EVANS',
+        image: './music/img/beoitutu.jpg',
+    },
 ]
