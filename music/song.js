3f4600de3528dfdf140273b577f35c0e58fa9de0
Hieuuu04 <minhhieu2004005@gmail.com>
2024-01-22 14:22:00 +0700

    fix song

--- music/song.js
+++ music/song.js
@@ -156,6 +156,24 @@ let songs = [
         image: '../music/img/thichemhoinhieu.jpg',
     },
     {
+        name: 'Bảo tàng',
+        path: '../music/baotang.mp3',
+        artist: 'Lowg',
+        image: '../music/img/baotang.jpg',
+    },
+    {
+        name: 'Càng Cua',
+        path: '../music/cangcua.mp3',
+        artist: 'Lowg',
+        image: '../music/img/cangcua.jpg',
+    },
+    {
+        name: 'okokokokoke',
+        path: '../music/okoookk.mp3',
+        artist: 'Lowg',
+        image: '../music/img/okok.jpg',
+    },
+    {
         name: 'Roar',
         path: '../music/roar.mp3',
         artist: 'Katy Perry',
@@ -233,6 +251,24 @@ let songs = [
         artist: 'Bray',
         image: '../music/img/xindungnhacmay.jpg',
     },
+    {
+        name: '2h',
+        path: '../music/2h.mp3',
+        artist: 'MCK',
+        image: '../music/img/2h.jpg',
+    },
+    {
+        name: 'Chỉ một đêm nữa thôi',
+        path: '../music/chi1demnuathoi.mp3',
+        artist: 'MCK',
+        image: '../music/img/chimotdemnuathoi.jpg',
+    },
+    {
+        name: 'Nghe như tình yêu',
+        path: '../music/nghenhutinhyeu.mp3',
+        artist: 'MCK',
+        image: '../music/img/nghenhutinhyeu.jpg',
+    },
 ];
 
 
@@ -262,11 +298,15 @@ let artistlist = [
         image: '../src/arimg/binz.jpg',
     },
     {
+        name: 'Lowg',
+        image: '../src/arimg/lowg.jpg',
+    },
+    {
         name: 'Maroon 5',
         image: '../src/arimg/maroon5.jpg',
     },
     {
-        name: 'Mck',
+        name: 'MCK',
         image: '../src/arimg/mck.jpg',
     },
     {
@@ -295,16 +335,118 @@ let pplSong = [
         image: '../music/img/kinhhong.jpg'
     },
     {
+        name: '0321',
+        path: '../music/0321.mp3',
+        artist: 'Spcae X',
+        image: '../music/img/0321.jpg'
+    },
+    {
+        name: 'Love Is Gone',
+        path: '../music/loveisgone.mp3',
+        artist: 'SLANDER',
+        image: '../music/img/loveisgone.jpg'
+    },
+    {
+        name: 'Arian',
+        path: '../music/arian.mp3',
+        artist: 'Arian',
+        image: '../music/img/arian.jpg'
+    }, 
+    {
+        name: 'Buồn hay vui',
+        path: '../music/buonhayvui.mp3',
+        artist: 'VSOUL x MCK',
+        image: '../music/img/buonhayvui.jpg'
+    }, 
+    {
+        name: 'Chẳng còn gì',
+        path: '../music/changcongi.mp3',
+        artist: 'Salmon',
+        image: '../music/img/Salmon.png'
+    }, 
+    {
+        name: 'Dân chơi sao phải khóc',
+        path: '../music/danchoi.mp3',
+        artist: 'Andree x Ryder',
+        image: '../music/img/danchoisaophaikhoc.jpg'
+    },
+    {
+        name: 'GREEN LOVE',
+        path: '../music/greenlove.mp3',
+        artist: 'QNT',
+        image: '../music/img/qnt.jpg'
+    }, 
+    {
         name: 'Lovin On Me',
         path: '../music/lovinonme.mp3',
         artist: 'Jack Harlow',
         image: '../music/img/lovinonme.jpg'
     }, 
     {
-        name: 'Love Is Gone',
-        path: '../music/loveisgone.mp3',
-        artist: 'SLANDER',
-        image: '../music/img/loveisgone.jpg'
+        name: 'Những lời hứa bỏ quên',
+        path: '../music/nhungloihua.mp3',
+        artist: 'Vũ',
+        image: '../music/img/nhungloihuaboquen.jpg'
+    },
+    {
+        name: 'Wn id 2022 ft 267',
+        path: '../music/id2022.mp3',
+        artist: 'W/n ',
+        image: '../music/img/wn.jpg'
+    },
+    {
+        name: 'Tại vì sao',
+        path: '../music/taivisao.mp3',
+        artist: 'MCK',
+        image: '../music/img/mck.jpg',
+    },
+    {
+        name: '100 Milion',
+        path: '../music/100MillionDIVINEKaranAujla.mp3',
+        artist: 'DIVINE KAran',
+        image: '../music/img/100milion.jpg',
+    },
+    {
+        name: 'Bé ơi từ từ',
+        path: '../music/beoitutu.mp3',
+        artist: 'WREN EVANS',
+        image: '../music/img/beoitutu.jpg',
+    },
+    {
+        name: 'Build a btch',
+        path: '../music/BellaPoarchBuildaBtch.mp3',
+        artist: 'Bella Poarch',
+        image: '../music/img/bellapoarch.jpg',
+    },
+    {
+        name: 'CHO MÌNH EM',
+        path: '../music/ChoMinhEm.mp3',
+        artist: 'BINZ X ĐEN',
+        image: '../music/img/binz.jpg',
+    },
+    {
+        name: 'NDA',
+        path: '../music/BillieEilishNDA.mp3',
+        artist: 'Bellie Eilish',
+        image: '../music/img/NDA.jpg',
+    },
+    {
+        name: 'As It ',
+        path: '../music/asit.mp3',
+        artist: 'Harry Styles',
+        image: '../music/img/harry styles-as it was.jpg',
+    },
+    {
+        name: 'JCHRIST',
+        path: '../music/LilNasXJCHRIST.mp3',
+        artist: 'Lil Nax X',
+        image: '../music/img/jchrist.jpg',
+    },
+    {
+        name: 'MONTERO',
+        path: '../music/LilNasXMONTEROCallMeByYourName.mp3',
+        artist: 'Lil Nas X',
+        image: '../music/img/montero.jpg',
     },
     {
         name: 'Old Town Road',
@@ -313,9 +455,147 @@ let pplSong = [
         image: '../music/img/lilnasx.jpg',
     },
     {
+        name: 'INDUSTRY',
+        path: '../music/LilNasXJackHarlowINDUSTRYBABY.mp3',
+        artist: 'Lil Nas X',
+        image: '../music/img/lilnasx and jackharlow.jpg',
+    },
+    {
+        name: 'Girls Like You ft Cardi B',
+        path: '../music/Maroon5.mp3',
+        artist: 'Maroon 5',
+        image: '../music/img/maroon5.jpg',
+    },
+    {
+        name: 'Nhắn nhủ',
+        path: '../music/nhannhu.mp3',
+        artist: 'Ronboongz',
+        image: '../music/img/ronboogz.jpg',
+    },
+    {
+        name: 'The Box',
+        path: '../music/RoddyRicchTheBox.mp3',
+        artist: 'Roddy Ricch',
+        image: '../music/img/thebox.jpg',
+    },
+    {
+        name: 'THÍCH EM HƠI NHIỀU',
+        path: '../music/thichemhoinhieu.mp3',
+        artist: 'WREN EVANS',
+        image: '../music/img/thichemhoinhieu.jpg',
+    },
+    {
+        name: 'Roar',
+        path: '../music/roar.mp3',
+        artist: 'Katy Perry',
+        image: '../music/img/katy perry- roar.jpg',
+    },
+    {
+        name: 'Lạc trôi',
+        path: '../music/lactroi.mp3',
+        artist: 'Sơn Tùng MTP',
+        image: '../music/img/lactroi.jpg',
+    },
+    {
+        name: 'Making My Way',
+        path: '../music/makingmyway.mp3',
+        artist: 'Sơn Tùng MTP',
+        image: '../music/img/makingmyway.jpg',
+    },
+    {
+        name: 'Nơi này có anh',
+        path: '../music/noinaycoanh.mp3',
+        artist: 'Sơn Tùng MTP',
+        image: '../music/img/sontungmtp.jpg',
+    },
+    {
+        name: 'Người con gái ta thương',
+        path: '../music/nguoicongaitathuong.mp3',
+        artist: 'Hà Anh Tuấn',
+        image: '../music/img/nguoicongaitathuong.jpg',
+    },
+    {
         name: 'Người tình mùa đông',
         path: '../music/nguoitinhmuadong.mp3',
         artist: 'Hà Anh Tuấn',
         image: '../music/img/nguoitinhmuadong.jpg',
+    },
+    {
+        name: 'Tháng mấy em nhớ anh',
+        path: '../music/thangmayemnhoanh.mp3',
+        artist: 'Hà Anh Tuấn',
+        image: '../music/img/thangmayemnhoanh.jpg',
+    },
+    {
+        name: 'Tháng Tư là lời nói dối của em',
+        path: '../music/thangtulaloinoidoicuaem.mp3',
+        artist: 'Hà Anh Tuấn',
+        image: '../music/img/thangtulaloinoidoicuaem.jpg',
+    },
+    {
+        name: 'タイムパラドックス',
+        path: '../music/jap.mp3',
+        artist: 'Various Artist',
+        image: '../music/img/disk.png',
+    },
+    {
+        name: 'Anh luôn như vậy',
+        path: '../music/anhluonnhuvay.mp3',
+        artist: 'Bray',
+        image: '../music/img/anhluonnhuvay.jpg',
+    },
+    {
+        name: 'Cao ốc',
+        path: '../music/caooc.mp3',
+        artist: 'Bray',
+        image: '../music/img/caooc.jpg',
+    },
+    {
+        name: 'Ex Hate Me',
+        path: '../music/exhateme.mp3',
+        artist: 'Bray',
+        image: '../music/img/exhateme.jpg',
+    },
+    {
+        name: 'Xin đừng nhấc máy',
+        path: '../music/xindungnhacmay.mp3',
+        artist: 'Bray',
+        image: '../music/img/xindungnhacmay.jpg',
+    },
+    {
+        name: '2h',
+        path: '../music/2h.mp3',
+        artist: 'MCK',
+        image: '../music/img/2h.jpg',
+    },
+    {
+        name: 'Chỉ một đêm nữa thôi',
+        path: '../music/chi1demnuathoi.mp3',
+        artist: 'MCK',
+        image: '../music/img/chimotdemnuathoi.jpg',
+    },
+    {
+        name: 'Nghe như tình yêu',
+        path: '../music/nghenhutinhyeu.mp3',
+        artist: 'MCK',
+        image: '../music/img/nghenhutinhyeu.jpg',
+    },
+    {
+        name: 'Bảo tàng',
+        path: '../music/baotang.mp3',
+        artist: 'Lowg',
+        image: '../music/img/baotang.jpg',
+    },
+    {
+        name: 'Càng Cua',
+        path: '../music/cangcua.mp3',
+        artist: 'Lowg',
+        image: '../music/img/cangcua.jpg',
+    },
+    {
+        name: 'okokokokoke',
+        path: '../music/okoookk.mp3',
+        artist: 'Lowg',
+        image: '../music/img/okok.jpg',
     },
 ]
