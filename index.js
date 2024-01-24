5f3e73143a0f7ecfdc80a1e25192e9b066ea1b5f
Trần Kim Cương <trankimcuong301@gmail.com>
2024-01-24 15:04:04 +0700

    login

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+const onUser = document.querySelector('#username');
+const onPass = document.querySelector('#password');
 
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
   
     function validate(){
-      if (username.value == "123" && password.value == "456") {
-        window.location.href = "./main/main.html";
-      } else {
-        alert("Đăng nhập thất bại. Vui lòng kiểm tra tên đăng nhập và mật khẩu.");
+      if (username.value == '' || password.value == '') {
+        alert("Vui lòng nhập tài khoản và mật khẩu");
+        return;
+      } 
+      else {
+        if (username.value == "123" && password.value == "456") {
+          window.location.href = "./main/main.html";
+        } else {
+          alert("Đăng nhập thất bại. Vui lòng kiểm tra tên đăng nhập và mật khẩu.");
+        }
       }
     }
   
+onPass.addEventListener('keypress', (event) => {
+  if (event.key === "Enter") {
+     validate();
+  }
+});
+onUser.addEventListener('keypress', (event) => {
+  if (event.key === "Enter") {
+     validate();
+  }
+});
